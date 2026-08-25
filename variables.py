fa281338ac6fbb50c04f92ddcea58ccb54153684
Qunduz Karimboyeva <karimboyevaqunduz544@gmail.com>
2026-08-25 05:20:51 +0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Pythonda variablesning qiymatini almashtirish
+a = int(input("a ning qiymatini kiriting:"))
+b = int(input("b ning qiymatini kiriting: "))
+(a, b) = (b, a) #O'zgarmas royxatdan foydalanamiz
+print("a ning yangi qiymati", a)
+print ("b ning yangi qiymati " , b)
+"""
+Output:
+	
+a ning qiymatini kiriting:5
+b ning qiymatini kiriting: 15
+a ning yangi qiymati 15
+b ning yangi qiymati  5
+"""
